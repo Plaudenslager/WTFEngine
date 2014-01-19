8837ab87d431a0c13ce7fefd781c89f08b992710
Peter Laudenslager <peter.laudenslager@gmail.com>
2014-01-18 22:40:01 -0800

    Modified for I Hurt Myself Again website; published on AWS S3

--- js/corpus.js
+++ js/corpus.js
@@ -15,52 +15,28 @@
  */
  
 var templates = [
-	"@language @technique framework that integrates with @social, built using the @pattern pattern",
-	"@language library for @social (ported from @language)",
-	"@social utility running on @os for the @api API",
-	"lightweight @format for converting @language @formats to @formats",
-	"lightweight @format for converting @language @formats to @formats - @os only",
-	"@adjective, @language based @api API explorer with a @technique front-end",
-	"@language based platform for rapidly developing @formats based on @api data",
-	"@language powered @social @format with @api integration for @os",
-	"@adjective tool for converting @language based @formats to @formats",
-	"@adjective @technique @format powered by the @api API",
-	"@adjective server-side @social aggregator for @platform",
-	"@adjective @technique @format powered by the @api API and @api",
-	"@os optimised @api configurator for @adjective @verb",
-	"@os optimised @api configurator for @adjective, @adjective @verb",
-	"@language interpreter for creating @adjective @verb experiences in @language",
-	"@technique @format that integrates @social and @social feeds (via @api)",
-	"@adjective @platform @format for @verb and @verb with @social data",
-	"@technique @verb platform for @language, @language and @language",
-	"platform for building @adjective, @adjective @formats for @platform enabled devices",
-	"@language based @social and @social mashup, designed for @adjective @verb",
-	"@language @pattern micro framework, optimised for generating @formats for @social users",
-	"@os specific @platform application to enable @social and @social users to leverage the @api API for @verb",
-	"@language based @format to replace @formats and hook into @api via @api and @api",
-	"@adjective, @adjective @social @verb utility powered by @social and the @api @language API",
-	"@format that generates poetry for @social users based on @api data",
-	"@format that generates poetry based on @api data",
-	"@language wrapper for @adjective @language @formats that utilises the @pattern pattern",
-	"@api powered @os emulator for @formats"
+	"While we were playing @sport @place, I was @action, and I @error, and hurt my @bodypart",
+	"We were watching @sport on TV, and I starting @action, and well, long story short, now my @bodypart hurts",
+	"My friend was @action, and I wanted to show her how to do it right, but I @error and injured my @bodypart",
+	"I think I need to learn how to play @sport @place without hurting my @bodypart every time"
 ];
 
 var phrases = [
-	"Get your coding shoes on and build a fucking",
-	"Don't need no UML diagram, just get cracking on a fucking",
-	"Get your shit together and get going with a fucking",
-	"This shit will be trending on Twitter - a fucking",
-	"Dinner is for the kind of chumps who couldn't build a fucking",
-	"Make some coffee; you're building a fucking",
-	"Get your fucking CPU crunching on a"
+	"O.K., you're never going to believe this, but",
+	"I know this sounds crazy that I did this again, but",
+	"I know I'm not supposed to, but",
+	"You know how today was supposed to be a resting day?  Well, it turns out that",
+	"Do we still have medical insurance?  I am only asking because",
+	"Don't tell my coach, but",
 ];
 
 var labels = [
-	"I've already fucking made that",
-	"That breaches a load of fucking copyrights",
-	"No one will fucking use that",
-	"That's not going to fucking work",
-	"Does not fucking compute"
+	"So anyway, thats why I have so many bags of ice",
+	"Do you think I will still be able to play in tomorrow's game?",
+	"I know, I know, you've told me a thousand times, blah, blah, blah",
+	"So now I'm not sure I can get my cleats on",
+	"So do you think I should tell my coach, or just see if it gets better?",
+	"I'm not sure, but I may have also popped out one of my stiches"
 ];
 
 /*
@@ -69,13 +45,8 @@ var labels = [
 
 corpus = {};
 
-corpus.language		 = ["Actionscript","Ajax","AS3","Boo","C","C#","C++","Delphi","Fortran","Haxe","HTML5","Java","JavaScript","Lingo","Lisp","Objective C","Pascal","Perl","PHP","Python","Ruby","Scala","Scheme","Visual BASIC"];
-corpus.platform		 = ["Adobe AIR","Arduino","Cinder","Flash","MATLAB","Open Frameworks","Processing"];
-corpus.api			 = ["Audio Scrobbler","Bing Maps","Blogger","Blogmarks","Delicious","Dictionary.com","Digg","Facebook","Feedburner","Flickr","Geocoder","GitHub","GMail","Google Analytics","Google App Engine","Google Blog Search","Google Books","Google Calendar","Google Code Search","Google Documents","Google Earth","Google Gears","Google Geocoding","Google Image Search","Google Language","Google Maps","Google Search","iTunes","Last.FM","LinkedIn","MediaWiki","NewsCloud","Snipplr","SoundCloud","Spotify","Twitter","vi.sualize.us","Vimeo","Weather.com","Wikipedia","Windows Live","Wink","Wordnik","Wordpress","Yahoo Answers","Yahoo Buzz","Yahoo Meme","Yahoo Music","Yahoo Pipes","Yahoo Shopping","Yahoo Weather","Yelp","YouTube"];
-corpus.os			 = ["Amiga","Android","Debian","Fedora","Google Chrome OS","iOS","Linux","Mac OSX","PS3 OS","Ubuntu","Windows"];
-corpus.pattern		 = ["Abstract Factory","Builder","Composite","Decorator","Factory Method","Multiton","MVC","Prototype","Proxy","Singleton"];
-corpus.social		 = ["Bebo","Blogmarks","Clipmarks","DailyBooth","Del.icio.us","Digg","Diigo","eBay","Facebook","Flickr","Foursquare","Furl","GitHub","Google Buzz","Google Buzz","Hyves","Last.FM","LinkedIn","LiveJournal","Myspace","Newsvine","Posterous","Reddit","Shelfari","Slashdot","SoundCloud","Spotify","StumbleUpon","Technorati","Tumblr","Twitter","vi.sualize.us","Vimeo","Wink","Wordpress","Yahoo Buzz","YouTube"];
-corpus.format		 = ["aggregator","AIR application","Android application","art installation","Blackberry application","bookmarklet","Chrome extension","dashboard widget","data visualisation","digital toy","experiment","Facebook application","Firefox plugin","Flash game","Google gadget","HTML5 game","installation","iPad application","iPhone application","kiosk","mashup","microsite","multiplayer game","music visualisation","physical installation","plugin","podcast","portal","prototype","robot","social game","visualisation","web application","web crawler","website"];
-corpus.technique	 = ["3D","face detecting","face tracking","feature detecting","gesture tracking","hand tracking","Kinect","Kinect hack","motion tracking","phishing","projection mapping","QR"];
-corpus.adjective	 = ["3D","audio reactive","collaborative","compressed","cross-platform","crowdsourced","distributed","dynamic","generative","gesture controlled","integrated","lightweight","locally executing","memcached","monetised","motion controlled","multi-touch","multicore","open source","optimised","plug & play","scalable","social","ubiquitous","viral","virtual","Wii controlled"];
-corpus.verb			 = ["blogging","browsing","chatting","file sharing","gaming","hacking","microblogging","sharing","shopping","syncing"];
+corpus.sport		= ["softball","football","volleyball","wall-ball","dodge-ball","checkers","tic-tac-toe","basketball","soccer"]
+corpus.place		= ["in the gym","on the playground","in the hallway","in the kitchen","at McDonalds","in my friends garage","on the roof"]
+corpus.action		= ["running","throwing","swinging a bat","pitching","passing the ball","trying to win","trying to grab the ball"]
+corpus.error		= ["fell","tripped","ran into a wall","crashed into my friend","got hit with the ball","hit myself","flipped upside down","heard something go snap"]
+corpus.bodypart		= ["eye","knee","ankle","leg","ear","finger","big toe","neck","elbow","thumb","shoulder",]
